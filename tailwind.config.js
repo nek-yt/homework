63e8857dca229633748d8a43de1838b107f4d3e4
nek-yt <googllgoog36@gmail.com>
2025-11-26 14:57:25 +0300

    section1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ }
